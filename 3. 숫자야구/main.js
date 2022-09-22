f073d60053a17ea38d11271a040cc8bb39ef608d
HOME\oh.jinyoung <oh_jinyoung@radcns.com>
2022-09-22 16:06:57 +0900

    2022-09-22 추가

--- 3. 숫자야구/main.js
+++ 3. 숫자야구/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
 
-import NumberBaseball from './NumberBaseball.vue'
+import NumberBaseball from './NumberBaseball'
 
-new Vue().$mount('#root');
+new Vue(NumberBaseball).$mount('#root');
--- 4. 반응속도체크/webpack.config.js
+++ 4. 반응속도체크/webpack.config.js
@@ -14,6 +14,12 @@ module.exports = {
         rules: [{
             test: /\.vue$/,
             loader: 'vue-loader',
+        }, {
+            test: /\.css$/,
+            use: [
+                'vue-style-loader',
+                'css-loader'
+            ]
         }],
     },
     plugins: [
@@ -22,5 +28,6 @@ module.exports = {
     output: {
         filename: '[name].js',
         path: path.join(__dirname, 'dist'),
+        publicPath: '/dist'
     },
 };
