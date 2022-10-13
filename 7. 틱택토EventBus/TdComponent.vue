2b31c69426e02d44c3db089f725844b9d3b6b7e0
HOME\oh.jinyoung <oh_jinyoung@radcns.com>
2022-10-13 16:51:59 +0900

    2022-10-13 추가

---
+++ 7. 틱택토EventBus/TdComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <td @click="onClickTd">{{cellData}}</td>
+</template>
+
+<script>
+import EventBus from './EventBus';
+export default {
+    props: {
+        cellData: String,
+        rowIndex: Number,
+        cellIndex: Number,
+    },
+    methods: {
+        onClickTd() {
+            if (this.cellData) return;
+            const rootData = this.$root.$data;
+            // console.log(this.$root.$data); // 최상위 데이터
+            // console.log(this.$parent.$data); // 부모 데이터
+            EventBus.$emit('clickTd', this.rowIndex, this.cellIndex);
+
+
+            
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
